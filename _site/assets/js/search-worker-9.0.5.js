66a9f85bc123df2a4884b0dcf73fb0b34289b9fb
Sue-Jeong, Kwon <sue11@snu.ac.kr>
2020-12-07 18:56:55 +0900

    .

--- _site/assets/js/search-worker-9.0.5.js
+++ _site/assets/js/search-worker-9.0.5.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-12-07T18:47:02+09:00';
+const INDEX_KEY = 'index--2020-12-07T18:55:47+09:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
