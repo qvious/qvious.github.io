7aa5d31e65efc622c7d4e7338200a865e5097597
suekwon <artlessart@naver.com>
2021-04-05 23:55:07 +0900

    .

--- _site/assets/js/search-worker-9.0.5.js
+++ _site/assets/js/search-worker-9.0.5.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2021-04-05T18:26:22+09:00';
+const INDEX_KEY = 'index--2021-04-05T23:53:32+09:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
