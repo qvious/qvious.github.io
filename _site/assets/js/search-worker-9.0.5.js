80196808e9ae0cf54571a9904e66721bd1fa2437
qvious <artlessart@naver.com>
2021-04-06 14:33:06 +0900

    .

--- _site/assets/js/search-worker-9.0.5.js
+++ _site/assets/js/search-worker-9.0.5.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2021-04-06T14:29:14+09:00';
+const INDEX_KEY = 'index--2021-04-06T14:31:31+09:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
