16421fbe7ce4d55bf3161b0392fcac325e1067fd
qvious <artlessart@naver.com>
2022-01-19 18:13:44 +0900

    .

--- build/assets/js/search-worker-9.0.5.js
+++ build/assets/js/search-worker-9.0.5.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2022-01-19T18:02:06+09:00';
+const INDEX_KEY = 'index--2022-01-19T18:12:42+09:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
